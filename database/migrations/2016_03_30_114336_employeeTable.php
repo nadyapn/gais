134072acb4ed3a010026778ce33c4d627230bf5f
Prima P. Lusiana <lusianaprima@gmail.com>
2016-03-31 00:59:39 +0700

    update table employee,selfservice,reimburse

--- database/migrations/2016_03_30_114336_employeeTable.php
+++ database/migrations/2016_03_30_114336_employeeTable.php
@@ -14,16 +14,16 @@ class EmployeeTable extends Migration
     {
         //
         Schema::create('employee', function (Blueprint $table) {
-            $table->string('id_employee');
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->string('division');
-            $table->string('postition');
-            $table->string('role');
-            $table->string('supervisor');
-            $table->primary('idemployee');
-            $table->foreign('supervisor')->references('idemployee')->on('employee')->onDelete('restrict')->onUpdate('cascade');
+            $table->string('id_employee', 30);
+            $table->string('name', 30);
+            $table->string('email', 30);
+            $table->string('password', 30);
+            $table->string('division', 30);
+            $table->string('position', 30);
+            $table->string('role', 30);
+            $table->string('supervisor', 30);
+            $table->primary('id_employee');
+            $table->foreign('supervisor')->references('id_employee')->on('employee')->onDelete('restrict')->onUpdate('cascade');
         });
     }
 
